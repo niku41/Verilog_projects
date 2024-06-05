3114bb44030966d03eafb518f9284cd06a21aa57
Nikita Dhuppe <110588643+niku41@users.noreply.github.com>
2024-06-05 12:11:24 +0530

    Add files via upload

---
+++ single_port_ram.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.03.2024 20:50:13
+// Design Name: 
+// Module Name: single_port_ram
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module single_port_ram(
+
+    );
+endmodule

22ea60e2d01e96097e3c2a3e8265504870217386
Nikita Dhuppe <110588643+niku41@users.noreply.github.com>
2024-06-05 12:06:32 +0530

    Add files via upload

---
+++ neg_edge_tff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 16:22:04
+// Design Name: 
+// Module Name: neg_edge_tff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module neg_edge_tff(
+
+    );
+endmodule
---
+++ tff_using_dff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.03.2024 16:22:22
+// Design Name: 
+// Module Name: tff_using_dff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tff_using_dff(
+
+    );
+endmodule
